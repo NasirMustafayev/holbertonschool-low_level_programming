eabe73851797c0176b207570a1eea4867bd9e22a
NasirMustafayev <nasir.mustafayev@gmail.com>
2025-12-02 23:23:49 +0400

    Update

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alpabet in lower but q,e out
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter == 'q' || letter == 'e')
+			continue;
+		putchar(letter);
+	}
+	putchar('\n');
+
+	return (0);
+}
---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the single digit numbers
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		printf("%i", num);
+	}
+	return (0);
+}
