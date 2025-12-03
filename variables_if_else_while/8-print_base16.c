44e901506aad367502e36e07ec6193f7824773a3
NasirMustafayev <nasir.mustafayev@gmail.com>
2025-12-03 16:35:48 +0400

    Update and fixes

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alpabet in lower and uppercase
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+	char letter;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar(num + '0');
+	}
+	for (letter = 'a'; letter <= 'f'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+
+	return (0);
+}
