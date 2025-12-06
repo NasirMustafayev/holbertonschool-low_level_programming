dbdab588b5321605a6c69db407b9d3aebee56991
NasirMustafayev <nasir.mustafayev@gmail.com>
2025-12-06 13:08:46 +0400

    Fix

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -12,14 +12,14 @@ void print_to_98(int n)
 	{
 		for (c = 0; c <= 98; c++)
 		{
-			(c == 98) ? printf("%i ", c) : printf("%i, ", c);
+			(c == 98) ? printf("%i", c) : printf("%i, ", c);
 		}
 	}
 	else
 	{
 		for (c = n ; c >= 98; c--)
 		{
-			(c == 98) ? printf("%i ", c) : printf("%i, ", c);
+			(c == 98) ? printf("%i", c) : printf("%i, ", c);
 		}
 	}
 	printf("\n");
