6b818c155cde89316dadb007055dda42180b9e60
NasirMustafayev <nasir.mustafayev@gmail.com>
2025-12-06 13:03:29 +0400

    Update

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - adds 2 numbers
+ * @n1: first number
+ * @n2: second number
+ *
+ * Return: the sum of n1 and n2
+ */
+int add(int n1, int n2)
+{
+	int calc = n1 + n2;
+
+	return (calc);
+}
