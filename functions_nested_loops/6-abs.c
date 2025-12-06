f61bbaab29f336f8f623d7d071f0f95a7b1356df
NasirMustafayev <nasir.mustafayev@gmail.com>
2025-12-06 10:12:10 +0400

    Update

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - Returns the absolute value of an integer
+ * @n: the number to check
+ *
+ * Return: the absolute value
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	else
+		return (n);
+}
