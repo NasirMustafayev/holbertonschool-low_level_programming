931502ea042385f1bd213de488e902c28929b653
NasirMustafayev <nasir.mustafayev@gmail.com>
2025-12-07 15:45:22 +0400

    Update

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * more_numbers - prints numbers from 0 to 14, 10 times
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int num, digit;
+
+	for (num = 0; num <= 10; num++)
+	{
+		for (digit = 0; digit <= 14; digit++)
+		{
+			if (digit >= 10)
+				_putchar((digit / 10) + '0');
+			_putchar((digit % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
