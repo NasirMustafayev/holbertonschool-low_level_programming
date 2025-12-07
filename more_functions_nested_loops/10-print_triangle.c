9eef3600a3c279762ba9a0dcc3493d32c567a8cc
NasirMustafayev <nasir.mustafayev@gmail.com>
2025-12-07 20:02:18 +0400

    Fix

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * print_triangle - Prints # to console n of times
- *@size: input number
+ *print_triangle - create triangle with # on console
+ *@size: number of times the '#' character should be printed
  *
  * Return: void
  */
@@ -9,6 +9,11 @@ void print_triangle(int size)
 {
 	int r, c;
 
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
 	for (r = 0; r < size; r++)
 	{
 		for (c = 0; c < size - r - 1; c++)
--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_square - create a square with # in console 
+ *print_square - create a square with # in console
  *@size: number of times the '#' character should be printed
  *
  * Return: void
@@ -9,6 +9,11 @@ void print_square(int size)
 {
 	int r, c;
 
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
 	for (r = 0; r < size; r++)
 	{
 		for (c = 0; c < size; c++)
