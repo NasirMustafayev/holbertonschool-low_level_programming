45bb8cc6ccddc39c74a019d78f85fedc7d299d63
NasirMustafayev <nasir.mustafayev@gmail.com>
2026-01-12 15:42:39 +0400

    Fix

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,23 +1,25 @@
 #ifndef DOG_H
 #define DOG_H
 /**
- * struct dog - A structure to represent a dog
- * @name: The name of the dog
- * @age: The age of the dog
- * @owner: The owner of the dog
+ * struct dog - structure for a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
  */
-typedef struct
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 } dog_t;
-struct dog
+/*
+ *  struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+*/
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
