a93427d0ef1e66398f3d4331b5b4271d9472bde2
NasirMustafayev <nasir.mustafayev@gmail.com>
2026-01-29 20:57:19 +0400

    Update

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -8,15 +8,11 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int i;
+	size_t i;
 
 	if (array == NULL || action == NULL)
 		return;
-	else
-	{
-		for (i = 0; i < size; i++)
-		{
-			action(array[i]);
-		}
-	}
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
 }
