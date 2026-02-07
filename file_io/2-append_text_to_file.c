9518a6ff0ebc49abdd863bd37d2a8c268c7d0ab8
NasirMustafayev <nasir.mustafayev@gmail.com>
2026-02-07 20:14:28 +0400

    Fix update, variable type Task3

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -15,7 +15,7 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int fileDescriptor;
 	ssize_t bytesWritten;
-	size_t length;
+	ssize_t length;
 
 	if (filename == NULL)
 		return (-1);
